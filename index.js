e1970bd9fd897be7af69b331cdc51620a2470766
Andrew Di Jorio <acdijorio@gmail.com>
2023-10-19 07:49:20 -0500

    code completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+//  for (let i = 0; i < gifts.length; i++) {
+ //   console.log(`Wrapped ${gifts[i]} and added a bow!`);
+ //   debugger;
+ // }
+//
+ // return gifts;
+//}
+function writeCards(names, event) {
+    for (let i = 0; i < names.length - 1; i++) {
+   return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+return names;
+}
+//let countDown = 10
+//for(let i = )
+
+
+//var countDown = 10;{
+//
+ //   while (countDown >= 0){
+ //       console.log(countDown);
+//        countDown = countDown - 1;
+//    }
+    
+//};
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number)
+        number = number -1
+    }
+
+
+};
